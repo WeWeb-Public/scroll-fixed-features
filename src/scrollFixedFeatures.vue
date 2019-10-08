c4052433d3efb3695975ac5d5656ccd7b7af3344
fbriou <fbriou@gmail.com>
2019-10-08 19:59:14 +0200

    almost done sticky and anim

--- src/scrollFixedFeatures.vue
+++ src/scrollFixedFeatures.vue
@@ -13,23 +13,48 @@
 
         <div class="content">
             <div ref="leftContent" class="left-content">
-                <div class="fixed-left">
-                    <div v-for="(feature, index) in features" :key="index" class="feature" :class="{'active': feature.active}">
-                        <div class="title">{{feature.title}}</div>
-                        <div class="details" :class="{'show': feature.active}">
-                            <wwLayoutColumn tag="div" ww-default="ww-text" :ww-list="feature.details" class="list" @ww-add="add(feature.details, $event)" @ww-remove="remove(feature.details, $event)">
-                                <wwObject tag="div" v-for="object in feature.details" :key="object.uniqueId" :ww-object="object"></wwObject>
-                            </wwLayoutColumn>
+                <div class="fixed-left-container">
+                    <div class="fixed-left">
+                        <div v-for="(feature, index) in features" :key="index" class="feature" :class="{'active': feature.active}">
+                            <div class="title">
+                                <div v-if="editMode" class="contextmenu-container">
+                                    <div class="index">{{index + 1}}</div>
+                                    <wwContextMenu class="contextmenu" tag="div" @ww-add-before="addFeature(index, 'before')" @ww-add-after="addFeature(index, 'after')" @ww-remove="removeFeature(index)">
+                                        <div class="wwi wwi-config"></div>
+                                    </wwContextMenu>
+                                </div>
+                                <div v-if="editMode" class="toggle-active">
+                                    <label>
+                                        <input type="checkbox" v-model="feature.active" />
+                                        <span class="check"></span>
+                                    </label>
+                                </div>
+                                <wwObject v-bind:ww-object="feature.title"></wwObject>
+                            </div>
+                            <div class="details" :class="{'show': feature.active}">
+                                <wwLayoutColumn tag="div" ww-default="ww-text" :ww-list="feature.details" class="list" @ww-add="add(feature.details, $event)" @ww-remove="remove(feature.details, $event)">
+                                    <wwObject tag="div" v-for="object in feature.details" :key="object.uniqueId" :ww-object="object"></wwObject>
+                                </wwLayoutColumn>
+                            </div>
                         </div>
+                    </div>
+                </div>
+                <div class="fixed-right-container">
+                    <div class="fixed-right">
+                        <wwObject v-bind:ww-object="features[activeFeatureIndex].rightBackground"></wwObject>
                     </div>
                 </div>
             </div>
             <div ref="rightContent" class="right-content">
-                <div>#READY MADE COMPONENTS</div>
-                <div ref="content_0" :style="{height:'400px', width: '100%', 'background-color': 'blue'}"></div>
-
-                <div ref="content_1" :style="{height:'400px', width: '100%', 'background-color': 'blue', 'margin-top':'300px'}"></div>
-                <div ref="content_2" :style="{height:'400px', width: '100%', 'background-color': 'blue', 'margin-top':'300px'}"></div>
+                <div class="content-container" :ref="`content_${index}`" v-for="(feature, index) in section.data.features" :key="index">
+                    <div v-if="editMode" class="contextmenu-container">
+                        <div class="index">{{index+1}}</div>
+                    </div>
+                    <wwLayoutColumn tag="div" ww-default="ww-text" :ww-list="feature.contents" class="list" @ww-add="add(feature.contents, $event)" @ww-remove="remove(feature.contents, $event)">
+                        <wwObject tag="div" v-for="object in feature.contents" :key="object.uniqueId" :ww-object="object"></wwObject>
+                    </wwLayoutColumn>
+                    <!-- <div ref="content_0" :style="{height:'400px', width: '100%', 'background-color': 'blue', 'margin-bottom':'300px'}"></div> -->
+                </div>
             </div>
         </div>
     </div>
@@ -48,23 +73,14 @@ export default {
     data() {
         return {
             activeFeatureIndex: 0,
+            test: false,
             features: [
                 {
-                    active: true,
-                    title: 'BUILD',
-                    details: [],
-                    contents: []
-                },
-                {
                     active: false,
-                    title: 'BUILD',
+                    title: null,
                     details: [],
-
-                },
-                {
-                    active: false,
-                    title: 'BUILD',
-                    details: [],
+                    contents: [],
+                    rightBackground: {}
                 }
             ],
             lastScrollTop: 0,
@@ -74,8 +90,10 @@ export default {
     computed: {
         section() {
             return this.sectionCtrl.get();
+        },
+        editMode() {
+            return this.sectionCtrl.getEditMode() == 'CONTENT'
         }
-
     },
     created() {
 
@@ -87,6 +105,27 @@ export default {
         if (!this.section.data.color) {
             this.section.data.color = wwLib.wwObject.getDefault({
                 type: 'ww-color'
+            });
+            needUpdate = true
+        }
+        this.features = this.section.data.features;
+        this.features[0].rightBackground = wwLib.wwObject.getDefault({
+            type: 'ww-image'
+        })
+        setTimeout(() => {
+            console.log(this.section.data.features)
+            this.section.data.features[0].active = true;
+        }, 5000);
+        if (!this.section.data.features[0].title) {
+            this.section.data.features[0].title = wwLib.wwObject.getDefault({
+                type: 'ww-text',
+                data: {
+                    text: {
+                        fr_FR: 'Hello World FR !',
+                        en_GB: 'Hello World !'
+                    }
+                }
+
             });
             needUpdate = true
         }
@@ -125,6 +164,35 @@ export default {
             try {
                 list.splice(options.index, 1);
                 this.sectionCtrl.update(this.section);
+            } catch (error) {
+                wwLib.wwLog.error('ERROR : ', error);
+            }
+        },
+        addFeature(_index, where) {
+            try {
+                const up = (where == 'after') ? parseInt(1) : 0;
+                const index = _index + up
+                const newFeature = this.getNewFeature()
+                this.section.data.features.splice(index, 0, newFeature);
+                this.sectionCtrl.update(this.section);
+                this.restartScrollListerner()
+            } catch (err) {
+                wwLib.wwLog.error('ERROR : ', error);
+            }
+        },
+        getNewFeature() {
+            const newFeature = JSON.parse(JSON.stringify(this.section.data.features[0]))
+            wwLib.wwUtils.changeUniqueIds(newFeature)
+            return newFeature
+        },
+        removeFeature(index) {
+            try {
+                this.section.data.features.splice(index, 1);
+                if (!this.section.data.features.length) {
+                    this.addFeature(0, 'after');
+                }
+                this.sectionCtrl.update(this.section);
+                this.restartScrollListerner()
             } catch (error) {
                 wwLib.wwLog.error('ERROR : ', error);
             }
@@ -192,45 +260,146 @@ export default {
 <!-- Add lang="scss" or others to use computed CSS -->
 <style lang="scss" scoped>
 .scroll-fixed-features {
-    padding: 100px 5%;
+    position: relative;
     pointer-events: all;
+    min-height: 100vh;
     .content {
         position: relative;
         .left-content {
             position: absolute;
-            width: 40%;
-            height: 100%;
-            padding-right: 80px;
-            .fixed-left {
-                position: sticky;
-                top: 50px;
-                .feature {
-                    margin-top: 20px;
-                    height: 28px;
-                    transition: height 0.6s;
-                    .details {
-                        transition: transform 0.6s, opacity 0.6s;
-                        transform: translate3d(0px, -10px, 0px) scale3d(1, 1, 1)
-                            rotateX(0deg) rotateY(0deg) rotateZ(0deg)
-                            skew(0deg, 0deg);
-                        transform-style: preserve-3d;
-                        opacity: 0;
-                        &.show {
-                            transform: translate3d(0px, 0px, 0px)
+            height: 100vh;
+            width: 100%;
+            .fixed-left-container {
+                position: absolute;
+                width: 35%;
+                height: 100%;
+                margin-left: 5%;
+                top: 0;
+                left: 0;
+                padding-right: 80px;
+                .fixed-left {
+                    position: sticky;
+                    top: 50px;
+                    .feature {
+                        margin-top: 20px;
+                        height: 28px;
+                        transition: height 0.6s;
+                        .details {
+                            transition: transform 0.6s, opacity 0.6s;
+                            transform: translate3d(0px, -10px, 0px)
                                 scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg)
                                 rotateZ(0deg) skew(0deg, 0deg);
-                            opacity: 1;
+                            transform-style: preserve-3d;
+                            opacity: 0;
+                            &.show {
+                                transform: translate3d(0px, 0px, 0px)
+                                    scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg)
+                                    rotateZ(0deg) skew(0deg, 0deg);
+                                opacity: 1;
+                            }
                         }
-                    }
-                    &.active {
-                        height: 200px;
+                        &.active {
+                            height: 200px;
+                        }
                     }
                 }
             }
+            .fixed-right-container {
+                position: absolute;
+                width: 60%;
+                height: 100%;
+                top: 0;
+                right: 0;
+                .fixed-right {
+                    position: sticky;
+                    top: 50px;
+                    width: 100%;
+                    right: 0;
+                }
+            }
+            .title {
+                position: relative;
+            }
+        }
+        .right-background {
+            z-index: 1;
+            position: absolute;
         }
         .right-content {
-            width: 60%;
+            z-index: 2;
+            position: relative;
+            width: 55%;
             margin-left: 40%;
+            .content-container {
+                position: relative;
+            }
+        }
+        .contextmenu-container {
+            position: absolute;
+            top: 0;
+            left: 0;
+            transform: translateY(-100%);
+            border-top-left-radius: 20px;
+            border-top-right-radius: 20px;
+            padding: 5px;
+            text-align: center;
+            background-color: #ef811a;
+            color: white;
+            .index {
+            }
+            .contextmenu {
+                font-size: 1.2rem;
+                cursor: pointer;
+                z-index: 2;
+            }
+        }
+        .toggle-active {
+            position: absolute;
+            top: 0;
+            left: 30px;
+            transform: translateY(-100%);
+            input[type="checkbox"] {
+                -webkit-appearance: none;
+                appearance: none;
+                visibility: hidden;
+                display: none;
+            }
+
+            .check {
+                position: relative;
+                display: block;
+                width: 40px;
+                height: 18px;
+                background-color: #ffffff;
+                border: 1px solid #19947c;
+                cursor: pointer;
+                border-radius: 20px;
+                overflow: hidden;
+                transition: ease-in 0.5s;
+            }
+
+            input:checked[type="checkbox"] ~ .check {
+                background-color: #19947c;
+                /*   box-shadow: 0 0 0 1200px #092c3e; */
+            }
+
+            .check:before {
+                content: "";
+                position: absolute;
+                top: 3px;
+                left: 4px;
+                background-color: #19947c;
+                width: 11px;
+                height: 11px;
+                border-radius: 50%;
+                transition: all 0.5s;
+            }
+
+            input:checked[type="checkbox"] ~ .check:before {
+                // transform: translateX(50px);
+                left: calc(100% - 15px);
+                background-color: #ffffff;
+            }
         }
     }
 }
